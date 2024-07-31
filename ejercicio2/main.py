6e276b452677de79e7fe3fbbd603291da26b6c68
Miguel <mitovarr@unal.edu.co>
2024-07-31 16:00:48 -0500

    main ejercicio2

---
+++ ejercicio2/main.py
@@ -0,0 +1,10 @@
+import funciones
+def main():
+    numeros1 = [5, 85, 423, -32]
+    numeros2 = [49, 87, 0, -6.6]
+    funciones.producto_sencillo(numeros1,numeros2)
+
+
+
+if (__name__ == "__main__"):
+    main()
---
+++ ejercicio3/main.py
@@ -0,0 +1,10 @@
+import funcion
+def main():
+    numeros1 = [5, 85, 423, -32]
+    numeros2 = [49, 87, 0, -6.6]
+    funcion.producto_doble(numeros1,numeros2)
+
+
+
+if (__name__ == "__main__"):
+    main()
