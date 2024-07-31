76af1c8fa261b9feedf664e703f4b3598b312fd6
Miguel <mitovarr@unal.edu.co>
2024-07-31 15:11:03 -0500

    completados

---
+++ main.py
@@ -0,0 +1,9 @@
+import promedioreal
+def main():
+    arreglo_reales=[14, 73/3, 28**2, 4.2, 9.1, 1.9999**3, 67*0.5, 85, 31, 56]
+    promedioreal.promediototal(arreglo_reales)
+
+
+
+if (__name__ == "__main__"):
+    main()
---
+++ promedioreal.py
@@ -0,0 +1,7 @@
+def promediototal(lista1):
+    suma = 0
+    for i in lista1:
+        suma += i
+
+    promedio_total = suma / len(lista1)
+    print(f"El promedio de {lista1}) es: {promedio_total}")
